1f8da3c73ec1ae053a8f3bdd9970eef1b720625f
parthjoshi91 <parthdhj@usc.edu>
2015-05-26 20:12:45 -0700

    New Changes

--- test.py
+++ test.py
@@ -17,7 +17,7 @@ def webcrawler(url):
                 if time.time()<t:
                     get_single_post_data(str(href.get('href')))
                 else:
-                    print(time.time()-start_t)
+                    #print(time.time()-start_t)
                     exit(0)
     for link in soup.findAll('li',{'class':'collapsable channel submenu'}):
         href=link.a
@@ -38,7 +38,7 @@ def webcrawler(url):
                 if time.time()<t:
                      new_link_post_data(new_l)
                 else:
-                    print(time.time()-start_t)
+                    #print(time.time()-start_t)
                     exit(0)
 
 
@@ -90,18 +90,10 @@ def get_single_post_data(url):
                     db.commit()
                     #print("ok")
                 else:
-                    print(time.time()-start_t)
+                    #print(time.time()-start_t)
                     exit(0)
             except:
                 db.rollback()
-        for link in soup.findAll('h1'):
-            href=link.a
-            if href!= None:
-                if time.time()<t:
-                    get_single_post_data(str(href.get('href')))
-                else:
-                    print(time.time()-start_t)
-                    exit(0)
 
 
 
@@ -117,7 +109,7 @@ def new_link_post_data(url):
                 if time.time()<t:
                     get_single_post_data(str(href.get('href')))
                 else:
-                    print(time.time()-start_t)
+                    #print(time.time()-start_t)
                     exit(0)
 
 
@@ -125,4 +117,4 @@ def new_link_post_data(url):
 
 
 webcrawler('http://mashable.com')
-print(time.time()-start_t)
+#(time.time()-start_t)
